ce5638fcc9eb07cd97f8eb923e72995a46330941
LawrenceXieZ <944015925@qq.com>
2021-05-21 10:44:40 +0800

    票务管理
    1.加入票务管理功能，可显示购票记录
    2.点击单条记录可显示详细信息
    3.详细信息可打印
    4.加入退票逻辑，比较购票时间与当前时间
    5.加入背景图 背景_静态_云朵
    6.按钮、样式等已注释

---
+++ CTS/CommonUser/CommonUser/ShowTicketInfo.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CommonUser
+{
+	/// <summary>
+	/// ShowTicketInfo.xaml 的交互逻辑
+	/// </summary>
+	public partial class ShowTicketInfo : Window
+	{
+		public ShowTicketInfo(Ticket ticket)
+		{
+			InitializeComponent();
+			numofTicket.Text = ticket.O_Id + ticket.T_Id + ticket.S_Id;
+			nameofmovie.Text = ticket.M_Name;
+			numofyingting.Text = ticket.T_Id;
+			numofseats.Text = ticket.S_Id;
+			begintime.Text = ticket.O_BeginTime;
+			endtime.Text = ticket.O_EndTime;
+		}
+
+		private void Button_Print_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
