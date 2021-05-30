7beea90c65b69ff432176c5936c4c34f31515589
张睿 <43677361+ZRE-ray@users.noreply.github.com>
2021-05-30 23:11:36 +0800

    图片转换

--- CTS/CTSServer/src/main/java/TransmissionUtils/PicturePhaser.java
+++ CTS/CTSServer/src/main/java/TransmissionUtils/PicturePhaser.java
@@ -1,12 +1,45 @@
 package TransmissionUtils;
 
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.FileImageOutputStream;
+import java.io.*;
+
 public class PicturePhaser {
     //读取图片
-    public static byte pictureToBytes(String filename) {
+    public static byte[] pictureToBytes(String filename) {
+
+        byte[] data = null;
+        FileImageInputStream input = null;
+        try {
+            input = new FileImageInputStream(new File(filename));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int numBytesRead = 0;
+            while ((numBytesRead = input.read(buf)) != -1) {
+                output.write(buf, 0, numBytesRead);
+            }
+            data = output.toByteArray();
+            output.close();
+            input.close();
+        }
+        catch (FileNotFoundException ex1) {
+            ex1.printStackTrace();
+        }
+        catch (IOException ex1) {
+            ex1.printStackTrace();
+        }
+        return data;
 
     }
     //字节流写图片文件
     public static void bytesToPicture(byte[] bytes) {
-
+        try{
+            FileImageOutputStream imageOutput = new FileImageOutputStream(new File("D:\\1\\M.jpg"));
+            imageOutput.write(bytes, 0, bytes.length);
+            imageOutput.close();
+        } catch(Exception ex) {
+            System.out.println("Exception: " + ex);
+            ex.printStackTrace();
+        }
     }
 }
--- CTS/CommonUser/Transmission/PicturePhaser.cs
+++ CTS/CommonUser/Transmission/PicturePhaser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,23 @@ namespace CommonUser.Transmission
 {
     class PicturePhaser
     {
-        public static byte PictureToBytes(String filename)
+        public static byte[] PictureToBytes(String filename)
         {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
 
+            byte[] buffer = new byte[fs.Length];
+
+            fs.Read(buffer, 0, buffer.Length);
+            fs.Close();
+
+            return buffer;
         }
         public static void BytesToPicture(byte[] bytes)
         {
-
+            FileStream fs = new FileStream("D:\\1\\M.jpg", FileMode.Create);
+            fs.Write(bytes, 0, bytes.Length);
+            fs.Flush();
+            fs.Close();
         }
     }
 }
