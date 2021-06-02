ae92af306e878cbacf69269d4d04137ed7ab3ddf
Kvalwalls <1970587024@qq.com>
2021-06-02 17:45:57 +0800

    SQL建表+SQL建触发器

--- CTS/SQL/CreateTable.sql
+++ CTS/SQL/CreateTable.sql
@@ -1,3 +1,11 @@
+DROP table if EXISTS T_IDK;
+DROP table if EXISTS T_Record;
+DROP table if EXISTS T_Seat;
+DROP table if EXISTS T_Onmovie;
+DROP table if EXISTS T_Theater;
+DROP table if EXISTS T_Movie;
+DROP table if EXISTS T_User;
+
 create table T_User (
 	U_Id varchar(20) not null primary key, 
 	U_Name varchar(20) not null,
@@ -5,6 +13,7 @@ create table T_User (
 	U_Access varchar(1) not null, 
 	U_Money float not null
 );
+
 create table T_Movie (
 	M_Id varchar(20) not null primary key,   
 	M_Name varchar(40) not null,
@@ -13,11 +22,13 @@ create table T_Movie (
 	M_Comment float not null,
 	M_Description Varchar(800) not null
 );
+
 create table T_Theater (
 	T_Id Varchar(20) not null primary key,
 	T_Type Varchar(1) not null,
 	T_Size Int not null
 );
+
 create table T_OnMovie (
 	O_Id Varchar(20) not null primary key,
 	M_Id Varchar(20) not null,
@@ -29,6 +40,7 @@ create table T_OnMovie (
 	foreign key(T_Id) references t_theater(T_Id),
 	index(O_Id)
 );
+
 create table T_Seat (
 	O_Id Varchar(20) not null,
 	S_Id Varchar(20) not null,
@@ -37,17 +49,19 @@ create table T_Seat (
 	foreign key(O_Id) references T_OnMovie(O_Id),
 	index (O_Id,S_Id)
 );
+
 create table T_Record (
 	U_Id Varchar(20) not null,
 	O_Id Varchar(20) not null,
 	S_Id Varchar(20) not null,
-	R_Time Varchar(25)  not null,
+	R_Time Varchar(25) not null,
 	R_Price Float not null,
 	R_Status Varchar(1) not null,
 	primary key(U_Id,S_Id,O_Id),
 	foreign key(U_Id) references T_User (U_Id),
 	foreign key(O_Id,S_Id) references T_Seat (O_Id,S_Id)
 );
+
 create table T_IDK ( 
 	Id varchar(20) not null primary key,
 	I_Key varchar(8) not null
